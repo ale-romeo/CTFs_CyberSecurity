bfc8af900850db40eed0fd175371566daf26954c
Alessandro Romeo <87212112+ale-romeo@users.noreply.github.com>
2023-02-23 20:47:24 +0100

    Update aes_crypt.py

--- aes_crypt.py
+++ aes_crypt.py
@@ -3,21 +3,19 @@ from pwn import *
 def rol_bytes(b):
 
     # Perform the shift operation
-    for i in range(0, len(b)):
-        print(b[i])
-        b[i] = (b[i] << i) | (b[i] >> (8 - i))
-
-    # Convert the bytearray back to a bytes object
-    return bytes(b)
+    for i in range(len(b)):
+        shift = i + 1
+        b[i] = ((b[i] << shift) & 0xff) | (b[i] >> (8 - shift))
+        print(shift, b[i])
 
 
 def main():
     # connect to the server
     b = bytearray(open('flag.txt.aes', 'rb').read())
-    print(b)
     rol_bytes(b)
-    res = b.decode()
-    print(res)
+    flag = bytes(b)
+    print(flag)
+    
 
 if __name__ == '__main__':
     main()
