5d283000239081a3755240ba3c5631901d30a505
Alessandro Romeo <87212112+ale-romeo@users.noreply.github.com>
2023-03-04 23:51:12 +0100

    Add files via upload

--- script.py
+++ script.py
@@ -4,21 +4,37 @@ from string import printable
 def main():
     r = remote("benchmark.challs.cyberchallenge.it", 9031)
     r.recvuntil(b":")
+    dict = {}
+
+    for _ in printable:
+        r.sendline(_)
+        ans = r.recvuntil(b"cycles").decode().split()
+        c = int(ans[4])
+        dict[_] = c
+        r.recvuntil(b":")
+        if _ == '~':
+            break
+    
     flag = "CCIT{"
+    fcc = 1231
+    x = 1200
     r.sendline(flag)
     ans = r.recvuntil(b"cycles").decode().split()
-    count = int(ans[4])
     r.recvuntil(b":")
 
     while True:
         for _ in printable:
             temp = flag + str(_)
+            fcctemp = fcc + dict[_]
             r.sendline(temp)
             ans = r.recvuntil(b"cycles").decode().split()
             tempcount = int(ans[4])
-            print(tempcount, temp)
-            flag = temp
-            count = tempcount
+            if tempcount - fcctemp == x:
+                flag = temp
+                print(flag)
+                fcc = fcctemp
+                x += 300
+                break
             r.recvuntil(b":")
 
 
