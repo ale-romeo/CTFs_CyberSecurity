a9ed372a5809d96e8caedeac083e39065bca51b1
Alessandro Romeo <87212112+ale-romeo@users.noreply.github.com>
2023-03-19 13:52:42 +0100

    Add files via upload

--- x.py
+++ x.py
@@ -1,19 +1,31 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytesseract
+from PIL import Image
+import io
+import re
 
 def main():
     url1 = 'http://ctf.b01lers.com:5125/'
+    driver = webdriver.Firefox()
     visited = set()
     url = url1
-
+    
     while True:
         if url not in visited:
             visited.add(url)
             print('Crawling: ', url)
-            r = requests.get(url)
-            soup = BeautifulSoup(r.content, 'html.parser')
-            link = soup.find('span').get_text()
+            driver.get(url)
+            link = driver.find_element(By.TAG_NAME, 'span').text
+            if '.' in link:
+                ss = driver.get_screenshot_as_png()
+                im = Image.open(io.BytesIO(ss))
+                text = pytesseract.image_to_string(im, lang='eng')
+                print(text)
+                link = re.search('"(.*)’', text).group(1)
             url = url1+link+'.html'
+
+    driver.quit()
 
 
 if __name__ == '__main__':
